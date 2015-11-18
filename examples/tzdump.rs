a8293c2e7d185fe8eac7597f6c02ede906282999
Ben S <ogham@bsago.me>
2015-11-18 17:34:07 +0000

    Use datetime's TimeType and FixedTimespanSet types
    datetime@e6012ba003a8f5949510dea0f9ff334234202b07 added the ability to have time zones created at runtime and used in the same way as any other time zone. This commit takes advantage of that by using the TimeType and FixedTimespanSet and FixedTimespan structures present in datetime, allowing compiled time zones to be used alongside the static zoneinfo ones.
    
    The tzdump example was a casualty of this, as we no longer have the ability to look inside time zones to get at their transitions (this is coming, though)

--- examples/tzdump.rs
+++ examples/tzdump.rs
@@ -6,6 +6,10 @@ use std::io::Read;
 use std::path::Path;
 
 
+// This example is broken until we have a way to get at the transitions in
+// time zone data directly. Right now it just does a Rust Debug dump of the
+// file...
+
 fn main() {
     for arg in env::args().skip(1) {
         match File::open(&Path::new(&arg)) {
@@ -13,7 +17,7 @@ fn main() {
                 let mut contents = Vec::new();
                 file.read_to_end(&mut contents).unwrap();
                 match zoneinfo_compiled::parse(contents) {
-                    Ok(tzdata) => tzdump(tzdata),
+                    Ok(tzdata) => println!("{:#?}", tzdata),
                     Err(e)     => println!("{}", e),
                 }
             },
@@ -22,12 +26,12 @@ fn main() {
     }
 }
 
-fn tzdump(mut tz: zoneinfo_compiled::TZData) {
-    tz.transitions.sort_by(|a, b| a.timestamp.cmp(&b.timestamp));
-
-    for t in tz.transitions {
-        let l = &*t.local_time_type;
-        println!("{:11?}: name:{:5} offset:{:5} DST:{:5} type:{:?}",
-                  t.timestamp, l.name, l.offset, l.is_dst, l.transition_type);
-    }
-}
+// fn tzdump(mut tz: zoneinfo_compiled::TZData) {
+//     tz.transitions.sort_by(|a, b| a.timestamp.cmp(&b.timestamp));
+//
+//     for t in tz.transitions {
+//         let l = &*t.local_time_type;
+//         println!("{:11?}: name:{:5} offset:{:5} DST:{:5} type:{:?}",
+//                   t.timestamp, l.name, l.offset, l.is_dst, l.transition_type);
+//     }
+// }
--- src/lib.rs
+++ src/lib.rs
@@ -3,73 +3,61 @@
 #![crate_type = "dylib"]
 
 //! This is a library for parsing compiled zoneinfo files.
-//!
-//! ## Example
-//!
-//! ```no_run
-//! use std::fs::File;
-//! use std::io::Read;
-//! use std::path::Path;
-//! use zoneinfo_compiled::parse;
-//!
-//! let path = Path::new("/etc/localtime");
-//! let mut contents = Vec::new();
-//! File::open(path).unwrap().read_to_end(&mut contents).unwrap();
-//! let tz = parse(contents).unwrap();
-//!
-//! for t in tz.transitions {
-//!     println!("{:?}", t);
-//! }
-//! ```
+
+use std::borrow::Cow;
+use std::convert::AsRef;
+use std::path::Path;
+use std::sync::Arc;
 
 extern crate byteorder;
-use std::sync::Arc;
+extern crate datetime;
+use datetime::zone::{TimeZone, TimeType, TimeZoneSource, FixedTimespan};
+use datetime::zone::runtime::{OwnedTimeZone, OwnedFixedTimespanSet};
 
 pub mod parser;
 pub use parser::Result;
 
+pub trait CompiledData {
+    fn parse(input: Vec<u8>) -> Result<TimeZone>;
+
+    fn from_file<P: AsRef<Path>>(path: P) -> Result<TimeZone> {
+        use std::io::{Read, BufReader};
+        use std::fs::File;
+
+        let f = try!(File::open(path));
+        let mut r = BufReader::new(f);
+        let mut contents: Vec<u8> = Vec::new();
+
+        try!(r.read_to_end(&mut contents));
+        let tz = try!(Self::parse(contents));
+        Ok(tz)
+    }
+}
+
+impl CompiledData for TimeZone {
+    fn parse(input: Vec<u8>) -> Result<TimeZone> {
+        let data = try!(parse(input));
+        let arc = Arc::new(data.time_zone);
+        let tz = TimeZone(TimeZoneSource::Runtime(arc));
+        Ok(tz)
+    }
+}
+
 
 /// Parsed, interpreted contents of a zoneinfo file.
-#[derive(PartialEq, Eq, Debug, Clone)]
+#[derive(PartialEq, Debug)]
 pub struct TZData {
 
     /// Vector of transitions that are described in this data.
-    pub transitions: Vec<Transition>,
+    pub time_zone: OwnedTimeZone,
 
     /// Vector of leap seconds that are described in this data.
     pub leap_seconds: Vec<LeapSecond>,
 }
 
 
-/// The 'type' of time that the change was announced in.
-#[derive(Debug, PartialEq, Eq, Clone)]
-pub enum TransitionType {
-
-    /// Standard Time ("non-summer" time)
-    Standard,
-
-    /// Wall clock time
-    Wall,
-
-    /// Co-ordinated Universal Time
-    UTC,
-}
-
-
-/// A time change specification.
-#[derive(Debug, PartialEq, Eq, Clone)]
-pub struct Transition {
-
-    /// Unix timestamp when the clocks change.
-    pub timestamp: i32,
-
-    /// The new description of the local time.
-    pub local_time_type: Arc<LocalTimeType>,
-}
-
-
 /// A leap second specification.
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(Debug, PartialEq)]
 pub struct LeapSecond {
 
     /// Unix timestamp at which a leap second occurs.
@@ -82,20 +70,20 @@ pub struct LeapSecond {
 
 /// A description of the local time in a particular timezone, during the
 /// period in which the clocks do not change.
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct LocalTimeType {
 
     /// The time zone abbreviation - such as "GMT" or "UTC".
     pub name: String,
 
     /// Number of seconds to be added to Universal Time.
-    pub offset: i32,
+    pub offset: i64,
 
     /// Whether to set DST.
     pub is_dst: bool,
 
     /// The current 'type' of time.
-    pub transition_type: TransitionType,
+    pub transition_type: TimeType,
 }
 
 
@@ -125,24 +113,27 @@ pub fn cook(tz: parser::TZData) -> Result<TZData> {
 
         let info = LocalTimeType {
             name:             try!(String::from_utf8(name_bytes)),
-            offset:           ltt.offset,
+            offset:           ltt.offset as i64,
             is_dst:           ltt.is_dst != 0,
             transition_type:  flags_to_transition_type(tz.standard_flags[i] != 0,
                                                        tz.gmt_flags[i]      != 0),
         };
 
-        local_time_types.push(Arc::new(info));
+        local_time_types.push(info);
     }
 
     // ...then, link each transition with the time type it refers to.
     for i in 0 .. tz.header.num_transitions as usize {
         let t = &tz.transitions[i];
+        let ltt = local_time_types[t.local_time_type_index as usize].clone();
 
-        let transition = Transition {
-            timestamp:        t.timestamp,
-            local_time_type:  local_time_types[t.local_time_type_index as usize].clone(),
+        let timespan = FixedTimespan {
+            offset: ltt.offset,
+            is_dst: ltt.is_dst,
+            name: Cow::Owned(ltt.name),
         };
 
+        let transition = (t.timestamp as i64, timespan);
         transitions.push(transition);
     }
 
@@ -156,8 +147,16 @@ pub fn cook(tz: parser::TZData) -> Result<TZData> {
         leap_seconds.push(leap_second);
     }
 
+    let first = transitions.remove(0);
+
     Ok(TZData {
-        transitions: transitions,
+        time_zone: OwnedTimeZone {
+            name: None,
+            fixed_timespans: OwnedFixedTimespanSet {
+                first: first.1,
+                rest: transitions,
+            }
+        },
         leap_seconds: leap_seconds,
     })
 }
@@ -168,10 +167,10 @@ pub fn cook(tz: parser::TZData) -> Result<TZData> {
 /// The transition type is stored as two separate flags in the data file. The
 /// first set comes completely before the second, so these can only be
 /// combined after the entire file has been read.
-fn flags_to_transition_type(standard: bool, gmt: bool) -> TransitionType {
+fn flags_to_transition_type(standard: bool, gmt: bool) -> TimeType {
     match (standard, gmt) {
-        (_,     true)   => TransitionType::UTC,
-        (true,  _)      => TransitionType::Standard,
-        (false, false)  => TransitionType::Wall,
+        (_,     true)   => TimeType::UTC,
+        (true,  _)      => TimeType::Standard,
+        (false, false)  => TimeType::Wall,
     }
 }
